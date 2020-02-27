3d4f9c977237107be26061d74889c226d9176f68
hpark0811 <hpark327@uwo.ca>
2020-02-27 00:50:40 -0500

    add client connection functionality

--- CPTPPacket.js
+++ CPTPPacket.js
@@ -1,5 +1,5 @@
 module.exports = {
-  createPacket: function(
+  createPacket: function (
     version,
     msgType, // 1: Welcome, 2: Redirect
     senderId,
@@ -10,30 +10,45 @@ module.exports = {
   ) {
     reserved = null; // not used for this assignment
 
-    const versionBuffer = Buffer.alloc(3, version);
-    const msgTypeBuffer = Buffer.alloc(1, msgType);
-    const senderBuffer = Buffer.alloc(4, senderId);
-    const numOfPeersBuffer = Buffer.alloc(4, numOfPeers);
-    const reservedBuffer = Buffer.alloc(4, reserved);
-    const peerPortBuffer = Buffer.alloc(4, peerPort);
-    const peerIPBuffer = Buffer.alloc(4, peerIP);
+    let versionBuffer = Buffer.alloc(3);
+    let msgTypeBuffer = Buffer.alloc(1);
+    let senderBuffer = Buffer.alloc(4, senderId.toString()); // name, which gets cut off after 4 letters
+    let numOfPeersBuffer = Buffer.alloc(4);
+    let reservedBuffer = Buffer.alloc(2);
+    let peerPortBuffer = Buffer.alloc(2);
+    let peerIPBuffer;
 
-    return numOfPeers === 0 ? 
-            // Packet doesn't need to include reserved, peer address if there are peers
-            Buffer.concat([
-              versionBuffer,
-              msgTypeBuffer,
-              senderBuffer,
-              numOfPeersBuffer
-            ]) : 
-            Buffer.concat([
-              versionBuffer,
-              msgTypeBuffer,
-              senderBuffer,
-              numOfPeersBuffer,
-              reservedBuffer,
-              peerPortBuffer,
-              peerIPBuffer
-            ]);
+
+    if (numOfPeers > 0) {
+      reservedBuffer = Buffer.alloc(2);
+      peerPortBuffer = Buffer.alloc(2);
+      peerIPBuffer = Buffer.from(
+        peerIP.split('.').map((str) => parseInt(str))
+      ); // Converting string IP address to array of unsigned int to fit in 4 bytes
+    }
+
+    versionBuffer.writeInt16BE(version);
+    msgTypeBuffer.writeInt8(msgType);
+    numOfPeersBuffer.writeInt32BE(numOfPeers);
+    reservedBuffer.writeInt16BE(reserved);
+    peerPortBuffer.writeUInt16BE(peerPort);
+
+    return numOfPeers === 0 ?
+      // Packet doesn't need to include reserved, peer address if there are peers
+      Buffer.concat([
+        versionBuffer,
+        msgTypeBuffer,
+        senderBuffer,
+        numOfPeersBuffer
+      ]) :
+      Buffer.concat([
+        versionBuffer,
+        msgTypeBuffer,
+        senderBuffer,
+        numOfPeersBuffer,
+        reservedBuffer,
+        peerPortBuffer,
+        peerIPBuffer
+      ]);
   }
 }
--- ClientsHandler.js
+++ ClientsHandler.js
@@ -12,41 +12,39 @@ class PeerInfo {
 
 let peerTable = [];
 let maxPeerCount;
+let sender;
 
 module.exports = {
-  init: function (maxCount) {
+  init: function (maxCount, s) {
     maxPeerCount = maxCount;
+    sender = s;
   },
   handleClientJoining: function (sock) {
-    let messageType;
+    // Creating packet to send
+    let cPTPPacket = CPTPPacket.createPacket(
+      3314,
+      peerTable.length < maxPeerCount ? 1 : 2,
+      sender,
+      (peerTable.length) ? 1 : 0, // For this assignment, only attach max of 1 peer address
+      null,
+      (peerTable.length) ? peerTable[0].port : null,
+      (peerTable.length) ? peerTable[0].IPv4 : null,
+    );
+    sock.write(cPTPPacket);
 
     // TABLE NOT FULL
     if (peerTable.length < maxPeerCount) {
       console.log('Connected from peer: ' + sock.remoteAddress + ':' + sock.remotePort);
       peerTable.push(new PeerInfo(sock.remoteAddress, sock.remotePort));
-      messageType = 1 // Welcome type
     }
     // TABLE FULL
     else {
       console.log('Peer table full: ' + sock.remoteAddress + ':' + sock.remotePort + ' redirected');
-      messageType = 2 // Redirect type
     }
 
-    sock.on('data', (res) => {
-      let cPTPPacket = CPTPPacket.createPacket(
-        3314,
-        messageType,
-        'p1', // TODO: TEST VALUE FIX TO FOLDER NAME
-        (peerTable.length) ? 1 : 0, // For this assignment, only attach max of 1 peer address
-        null,
-        (peerTable.length) ? peerTable[0].port : null,
-        (peerTable.length) ? peerTable[0].IPv4 : null,
-      );
-      sock.write(cPTPPacket);
-    });
-    
+    // Handle disconnection
     sock.on('close', (res) => {
-      console.log('CLOSED: ' + sock.remoteAddress +':'+ sock.remotePort);
+      console.log('CLOSED: ' + sock.remoteAddress + ':' + sock.remotePort);
       peerTable = peerTable.filter((peer) => {
         return !(peer.IPv4 == sock.remoteAddress && peer.port == sock.remotePort);
       })
